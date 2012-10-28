84b69381e758d7744e7803ae95e3f6a7574737ca
Jorin Vogel <jorin.vogel@gmail.com>
2012-10-29 00:00:18 +0100

    implemented basic front-end

--- js/app.js
+++ js/app.js
@@ -1,17 +1,162 @@
 $(function() {
 
+  var $mail = $('#mail');
+  var $selectedInterval = $('#selected-interval');
+  var $intervals = $('#intervals').find('li');
+  var $current = $intervals.filter('.active');
+  var $download = $('#download');
+  var $leave = $('#leave');
+  var $submit = $('#submit');
+  var $success = $('#success');
+  var $error = $('#error');
+  var $controlGroup = $('#control-group');
+  var $introLink = $('#intro-link');
+  var $backupLink = $('#backup-link');
 
   remoteStorage.onWidget('ready', function() {
-    console.log('ready')
-    $('.container').addClass('right');
+    showView('backup');
+    $backupLink.show();
+    $.post('lookup', getData(), function(res) {
+      if (res) {
+        $mail.val(res.mail);
+        $intervals.removeClass('active');
+        //TODO: select current interval
+        $leave.removeClass('disabled');
+        $submit.text('update');
+      }
+    });
   });
 
   remoteStorage.onWidget('state', function(state) {
-    console.log('state', state)
-    if(state == 'disconnected') {
+    if(state === 'disconnected') {
+      showView('intro');
+      $backupLink.hide();
+      clear();
     }
   });
 
+  remoteStorage.claimAccess({'root':'rw'});
   remoteStorage.displayWidget('remotestorage-connect');
-  remoteStorage.util.silenceAllLoggers();
+  // remoteStorage.util.silenceAllLoggers();
+  // remoteStorage.root.setOnChange(console.log);
+  // remoteStorage.root.use('/');
+
+  $('#brand').click(function() {
+    showView('intro');
+  });
+
+  $introLink.click(function() {
+    showView('intro');
+  });
+
+  $backupLink.click(function() {
+    showView('backup');
+  });
+
+  $mail.keyup(function() {
+    $controlGroup[validMail() ? 'removeClass' : 'addClass']('error');
+  });
+
+  $intervals.click(function(e) {
+    e.preventDefault();
+    $intervals.removeClass('active');
+    $current = $(e.currentTarget);
+    $current.addClass('active');
+    $selectedInterval.text($current.text());
+  });
+
+  $submit.click(function() {
+    if (!validMail()) {
+      error('Please use a valid mail adress.');
+      return;
+    }
+    $.ajax({
+      type: 'POST',
+      url: '/update',
+      data: getData(),
+      success: function(res) {
+        $leave.removeClass('disabled');
+        success(res);
+        $submit.text('update');
+      },
+      error: function(res) {
+        error(res);
+      }
+    });
+  });
+
+  $download.click(function() {
+    //TODO: implement download
+  });
+
+  $leave.click(function() {
+    if ($leave.hasClass('disabled')) return;
+    $.ajax({
+      type: 'POST',
+      url: '/leave',
+      data: getData(),
+      success: function(res) {
+        clear();
+        success(res);
+        $leave.addClass('disabled');
+      },
+      error: function(res) {
+        error(res);
+      }
+    });
+  });
+
+  function showView(view) {
+    if (view === 'intro') {
+      $backupLink.removeClass('active');
+      $introLink.addClass('active');
+      $('.container').removeClass('right');
+    } else if ( view === 'backup') {
+      $introLink.removeClass('active');
+      $backupLink.addClass('active');
+      $('.container').addClass('right');
+    }
+  }
+
+  function getData(what) {
+    var data = {
+      //TODO: remove line as soon as bearer token works
+      bearerToken: '4eb4b398c36e62da87469133e2f0cb3f9574d5b3865051',
+      // bearerToken: remoteStorage.getBearerToken(),
+      storageHref: remoteStorage.getStorageHref()
+    };
+    if (what === 'all') {
+      data.mail = $mail.val();
+      data.interval = $current.find('a').attr('href').slice(1);
+    }
+    return data;
+  }
+
+  function clear() {
+    $mail.val('');
+    $intervals
+      .removeClass('active')
+      .eq(3)
+      .addClass('active');
+    $selectedInterval.text('daily');
+    $submit.text('subscribe');
+  }
+
+  function success(msg) {
+    $success.text(msg);
+    $error.hide();
+    $success.show();
+  }
+
+    function error(msg) {
+    $error.text(msg);
+    $success.hide();
+    $error.show();
+  }
+
+  var mailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  function validMail() {
+    return mailRegEx.test($mail.val());
+  }
+
 });
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost', 'rs-backup');
+var User;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  var userSchema = mongoose.Schema({
+    mail: String,
+    bearerToken: String,
+    storageHref: String,
+    interval: String
+  });
+  User = db.model('User', userSchema);
+});
 
 app.configure(function() {
   app.set('views', '');
@@ -7,7 +20,7 @@ app.configure(function() {
     layout: false
   });
   app.engine('html', require('ejs').renderFile);
-  // app.use(express.bodyParser());
+  app.use(express.bodyParser());
   // app.use(express.methodOverride());
   // app.use(app.router);
   app.use(express.static(__dirname));
@@ -15,10 +28,89 @@ app.configure(function() {
   app.use(express.compress());
 });
 
-app.get('/', function(req, res){
+
+app.get('/', function(req, res) {
   res.render('index.html');
 });
 
+app.post('/lookup', function(req, res) {
+  var data = req.body;
+  User.findOne(match(data), function(err, doc) {
+    if (doc === null) return;
+    data.mail = doc.mail;
+    data.inteval = doc.interval;
+    res.send(data);
+  });
+});
+
+app.post('/update', function(req, res) {
+  var data = req.body;
+  //TODO: validate data here
+  User.findOne(match(data), function(err, doc) {
+    var user = (doc === null) ? new User() : doc;
+    user.mail = data.mail;
+    user.interval = data.interval;
+    user.storageHref = data.storageHref;
+    user.bearerToken = data.bearerToken;
+    user.save(function (err) {
+      if (err) {
+        res.send(500, 'We are sorry. Something went wrong.');
+      } else {
+        res.send(doc === null ?
+          'Subscribtion was successful.'
+        : 'Settings were successfully updated.'
+        );
+      }
+    });
+  });
+});
+
+//TODO: remove /download
+app.post('/download', function(req, res) {
+  var data = req.body;
+  User.findOne({storageHref: new RegExp('^'+data.storageHref+'$', "i")}, function(err, res) {
+    if (res === null) {
+      return;
+    }
+
+    remoteStorage.nodeConnect.setUserAddress(data.storageHref, function(err) {
+      if(!err) {
+        remoteStorage.nodeConnect.setBearerToken(data.bearerToken);
+        console.log("rs connected!");
+        remoteStorage.claimAccess('root', 'r');
+      }
+    });
+
+    // res.attachment('tmp/' + filename);
+  });
+
+});
+
+app.post('/leave', function(req, res) {
+  var data = req.body;
+  User.findOne(match(data), function(err, doc) {
+    if (doc === null) {
+      res.send(404, 'We are sorry. Something went wrong.');
+      return;
+    }
+    doc.remove(function(err) {
+      if (err) {
+        console.log(err);
+        res.send(500, 'We are sorry. Something went wrong.');
+      } else {
+        res.send(200, 'Your data was successfully removed from rs-backup.');
+      }
+    });
+  });
+});
+
+function match(data) {
+  return {
+    storageHref: new RegExp('^'+data.storageHref+'$', "i"),
+    bearerToken: new RegExp('^'+data.bearerToken+'$', "i")
+  };
+}
+
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log('Listening on port 3000');
+console.log('Listening on port ', port);
