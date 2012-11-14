afb6baba3fb37a573bea21e70a894ffbdf745314
Jorin Vogel <jorin.vogel@gmail.com>
2012-11-14 17:41:30 +0100

    bla

--- server.js
+++ server.js
@@ -222,28 +222,38 @@ function buildData(zip, base, path) {
 function sendMail(optn) {
   var d = new Date();
   var date = d.toDateString() + ' - ' + d.toLocaleTimeString();
-  console.log('Send mail to ' + optn.user.mail + '\nFilename: rs-backup ' + date + '.zip\n' );
-  // console.log(optn.data)
-  transport.sendMail({
-      from: 'rs backup <remotestore.backup@gmail.com>',
-      to: optn.user.mail,
-      subject: 'rs backup at ' + date,
-      html:
-        "Here is the remoteStorage backup for " + optn.user.storageHref + ". \
-        To change your settings go to <a href=\"http://rs-backup.herokuapp.com\">rs-backup.herokuapp.com</a>.",
-      generateTextFromHTML: true,
-      attachments: [{
-        fileName: 'rs-backup.zip',
-        // fileName: 'rs-backup ' + date + '.zip',
-        contents: optn.data
-      }]
-  }, function(error, response) {
-    if (error) {
-      console.log('Send mail failure: ', error);
-    } else {
-      console.log("Message sent: " + response.message);
+  var path = 'tmp/rs-backup ' + date '.zip';
+  console.log('Send attachment from ' + path + '\n');
+  fs.writeFile(path, optn.data, 'binary', function(err) {
+    if (err) {
+      console.log('Error writing file to tmp/', err);
+      return;
     }
+    transport.sendMail({
+        from: 'rs backup <remotestore.backup@gmail.com>',
+        to: optn.user.mail,
+        subject: 'rs backup at ' + date,
+        html:
+          "Here is the remoteStorage backup for " + optn.user.storageHref + ". \
+          To change your settings go to <a href=\"http://rs-backup.herokuapp.com\">rs-backup.herokuapp.com</a>.",
+        generateTextFromHTML: true,
+        attachments: [{
+          filePath: 'path'
+        }]
+    }, function(error, response) {
+      fs.unlink(file, function (err) {
+        if (err) throw err;
+        console.log('successfully deleted ' + file);
+      });
+      if (error) {
+        console.log('Send mail failure: ', error);
+      } else {
+        console.log("Message sent: " + response.message);
+      }
+    });
+
   });
+
 }
 
 function interate(arr, fn) {
